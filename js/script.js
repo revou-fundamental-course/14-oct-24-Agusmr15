b536cf03bbeb03e4c9e538d2816fbfc10421f856
Agusmr15 <maulanaagus774@gmail.com>
2024-10-22 22:41:01 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
 // ini javascrit
-//javascript
+
+function replaceName() {
+    let name = prompt("Siapa nama anda?", "");
+    document.getElementById("name").innerHTML = name
+}
+
+document.getElementById('tombol').addEventListener("click", function() {
+    replaceName()
+})
+
+function validateForm() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const birtDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const message = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || messages =="") {
+        alert("Tidak boleh ada yang kosong");
+        return false;
+    }
+
+    setSenderUI(name, birthDate, gender, messages);
+
+    return false;
+
+    function setSenderUI(name, birtDate, gender, messages) {
+        document.getElementById("sender-full-name").innerHTML = name;
+        document.getElementById("sender-birth-date").innerHTML = name;
+        document.getElementById("sender-gender").innerHTML = name;
+        document.getElementById("sender-messages").innerHTML = name;
+    }
+}
